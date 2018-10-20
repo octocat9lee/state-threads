8ed3a2d485d691b5a71d7adabba53cccdaffb1be
zhoulee <295861542@qq.com>
2018-10-20 09:30:44 +0800

    编译示例

---
+++ examples/vars.mk
@@ -0,0 +1,9 @@
+# https://coolshell.cn/articles/3790.html
+%:
+	@echo '$*=$($*)'
+
+d-%:
+	@echo '$*=$($*)'
+	@echo '  origin = $(origin $*)'
+	@echo '   value = $(value  $*)'
+	@echo '  flavor = $(flavor $*)'
